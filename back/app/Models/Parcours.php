dfdb156617132e0ac8a58871ebf391062a588584
tahinaniaina01 <tahinaniaina01@gmail.com>
2024-04-26 16:19:35 +0300

    renomer les fichiers et les class models

--- back/app/Models/Parcours.php
+++ back/app/Models/Parcours.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class parcoursBDD extends Model
+class Parcours extends Model
 {
     protected $table = 'parcours';
     protected $primarykey = 'id';
--- back/app/Models/Personnes.php
+++ back/app/Models/Personnes.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class personnesBDD extends Model
+class Personnes extends Model
 {
     protected $table = 'personnes';
     protected $primarykey = 'id';
